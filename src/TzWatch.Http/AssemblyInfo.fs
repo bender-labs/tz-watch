7211bd0c6ed8354bc2a5552d3beacbfc8bc8f516
Jean-Baptiste Dusseaut <jb@benderlabs.io>
2021-04-08 17:43:28 +0200

    Bump version to 0.1.1
    ## [0.1.1] - 2021-04-08
    
    ### Change
    * bumps dependencies version

--- src/TzWatch.Http/AssemblyInfo.fs
+++ src/TzWatch.Http/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("TzWatch.Http")>]
 [<assembly: AssemblyProductAttribute("TzWatch")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-03-01T00:00:00.0000000+01:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-04-08T00:00:00.0000000+02:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.1")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","8ee47e8d97d17afa6992b6debdf745bbe95f79a5")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","dced9cf1d38fe8ed034e3670504199f200fdf55d")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "TzWatch.Http"
     let [<Literal>] AssemblyProduct = "TzWatch"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-03-01T00:00:00.0000000+01:00"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "0.1.1"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-04-08T00:00:00.0000000+02:00"
+    let [<Literal>] AssemblyFileVersion = "0.1.1"
+    let [<Literal>] AssemblyInformationalVersion = "0.1.1"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "8ee47e8d97d17afa6992b6debdf745bbe95f79a5"
+    let [<Literal>] AssemblyMetadata_GitHash = "dced9cf1d38fe8ed034e3670504199f200fdf55d"
