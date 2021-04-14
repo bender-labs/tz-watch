0b6fe35b543dce9591c44d1d476de77c5bcc6a03
Jean-Baptiste Dusseaut <jb@benderlabs.io>
2021-04-14 17:48:43 +0200

    Bump version to 0.2.0
    ## [0.2.0] - 2021-04-14
    
    ### Changed
    
    * Subscription does not return directly a JToken for parameters, but raw json payload
    
    ### Fixed
    
    * Tz-Watch does not die on big block

--- src/TzWatch.Lib/AssemblyInfo.fs
+++ src/TzWatch.Lib/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("TzWatch.Lib")>]
 [<assembly: AssemblyProductAttribute("TzWatch")>]
-[<assembly: AssemblyVersionAttribute("0.1.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-04-08T00:00:00.0000000+02:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.1")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-04-14T00:00:00.0000000+02:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","dced9cf1d38fe8ed034e3670504199f200fdf55d")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","ca92b964f16f58ebfd3284bd476273a81b80afc9")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "TzWatch.Lib"
     let [<Literal>] AssemblyProduct = "TzWatch"
-    let [<Literal>] AssemblyVersion = "0.1.1"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-04-08T00:00:00.0000000+02:00"
-    let [<Literal>] AssemblyFileVersion = "0.1.1"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.1"
+    let [<Literal>] AssemblyVersion = "0.2.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-04-14T00:00:00.0000000+02:00"
+    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.2.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "dced9cf1d38fe8ed034e3670504199f200fdf55d"
+    let [<Literal>] AssemblyMetadata_GitHash = "ca92b964f16f58ebfd3284bd476273a81b80afc9"
